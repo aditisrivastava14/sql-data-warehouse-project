795550a1935a3572f88de46e2b2a15f3ac7ae412
Aditi <aditisrivastava202@gmail.com>
2025-09-08 18:14:36 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,9 +1,14 @@
+-- Switch to system DB
 use mysql;
 
+-- Create a Data Warehouse "container" database 
 create database DataWarehouse;
 
+-- Switch to system DataWarehouse
 use DataWarehouse;
 
+-- Create schemas as separate databases
 CREATE SCHEMA bronze;
 CREATE SCHEMA silver;
 CREATE SCHEMA gold;
+
